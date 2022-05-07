6076eaa55f440dc7e237574c08be522213a4610a
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-07 14:07:42 -0400

    added api routes for GET and POST NOTE

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
+const db = require('../../db/db.json');
+const { createNewNote } = require('../../lib/notes');
 
+
+router.get('/notes', (req, res) => {
+    let results = db;
+    if (results) {
+        res.json(results);
+    } 
+    else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = db.length.toString();
+
+    const note = createNewNote(req.body, db);
+    res.json(note);
+    
+});
 
 module.exports = router;
