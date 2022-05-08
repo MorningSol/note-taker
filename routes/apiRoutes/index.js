0ac130c01bec2ff9ca976823dc9339f4dd2edd3d
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-08 10:57:21 -0400

    add heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,22 +1,22 @@
 const router = require('express').Router();
-const db = require('../../db/db.json');
+const  notes  = require('../../db/db.json');
 const { createNewNote } = require('../../lib/notes');
 
 
 router.get('/notes', (req, res) => {
-    let results = db;
+    let results = notes;
     if (results) {
         res.json(results);
     } 
     else {
-        res.send(404);
+        res.sendStatus(404);
     }
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = db.length.toString();
+    req.body.id = notes.length.toString();
 
-    const note = createNewNote(req.body, db);
+    const note = createNewNote(req.body, notes);
     res.json(note);
     
 });
